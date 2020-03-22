f25e3195d1b9d8f495428b2ea9b201720ff3b3c3
jinchangxfd <1660857877@qq.com>
2020-03-22 22:47:10 +0800

    test

--- src/app.js
+++ src/app.js
@@ -3,14 +3,23 @@ const fs =require('fs')
 const app = express()
 const port = 3000
 
-app.locals.dataFilePath = "./data.json"
+const {
+    getAccount,
+    getAllAccounts,
+    createAccount,
+    updateAccount,
+    deleteAccount
+} = require('./controller')
 
-app.get('/accounts', (req, res) => fs.readFile('./data.json','utf-8',(err,data)=>{
+app.get("/accounts/:id", getAccount)
+app.get("/accounts", getAllAccounts)
+
+/*app.get('/accounts', (req, res) => fs.readFile('./data.json','utf-8',(err,data)=>{
     if(err){
         res.status(500).send()
     }else{
         res.send(JSON.parse(data))
     }
-}))
+}))*/
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
